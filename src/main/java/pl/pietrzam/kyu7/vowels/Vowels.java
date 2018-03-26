c68c5716c6348f17919eb9d90f1f3cb0c32f16dd
Maciej Pietrzak <maciek1pietrzak@gmail.com>
2018-03-26 05:46:48 +0200

    Vowels - clever one

--- src/main/java/pl/pietrzam/kyu7/vowels/Vowels.java
+++ src/main/java/pl/pietrzam/kyu7/vowels/Vowels.java
@@ -1,23 +1,14 @@
 package pl.pietrzam.kyu7.vowels;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.regex.Pattern;
 
 class Vowels {
 
-    private static Set<Integer> VOWELS = new HashSet<>(5);
-
-    static {
-        VOWELS.add((int) 'a');
-        VOWELS.add((int) 'e');
-        VOWELS.add((int) 'i');
-        VOWELS.add((int) 'o');
-        VOWELS.add((int) 'u');
-    }
-
+    private static final Pattern VOWELS_PATTERN = Pattern.compile("(?i)[^aeiou]");
+    
     public static int getCount(final String str) {
-        return (int) str.chars()
-                            .filter(i -> VOWELS.contains(i))
-                            .count();
+        return VOWELS_PATTERN.matcher(str)
+                             .replaceAll("")
+                             .length();
     }
 }
